a714ea29a7e365fb869980d8cbf9470f2ec1067e
Karan Misal <143802033+karan18misal@users.noreply.github.com>
2025-03-05 20:57:02 +0530

    Create spammer.py

---
+++ spammer.py
@@ -0,0 +1,47 @@
+from sprite import Sprite
+def sprites_spam():
+    Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\house1.png',500,1300)
+    Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\house2.png', 500, 950)
+    Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\house3.png', 850, 1300)
+    Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\house4.png', 850, 950)
+    Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\house1.png', 3500, 1300)
+    Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\house2.png', 3500, 950)
+    Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\house3.png', 3850, 1300)
+    Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\house4.png', 3850, 950)
+    Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\house1.png', 3500, 100)
+    Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\house3.png', 3850, 100)
+    for i in range(0,61):
+        Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\tree.png', i*100, -64)
+    for i in range(0, 7):
+        Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\tree.png', 1000, 72*i)
+        Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\tree.png', 0, 72 * i)
+    for i in range(0,3):
+        Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\tree.png', i * 100, 720)
+    for i in range(5,30):
+        Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\tree.png', i * 100, 720)
+    for i in range(31,61):
+        Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\tree.png', i * 100, 720)
+    for i in range(1,11):
+        Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\tree.png', 1800+(i*100), 936)
+    for i in range(1, 4):
+        Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\tree.png', 2900, 720+(i*72))
+    for i in range(6, 12):
+        Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\tree.png', 2900, 720+(i*72))
+    for i in range(1, 3):
+        Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\tree.png', 2900-(i*100), 720+(6*72))
+    for i in range(5, 10):
+        Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\tree.png', 2900-(i*100), 720+(6*72))
+    for i in range(4, 7):
+        Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\tree.png', 2500, 720+(i*72))
+    for i in range(3,10):
+        Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\tree.png', 1800, 720+(i*72))
+    for i in range(6,9):
+        Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\tree.png', 1800+(2*100), 720+(i*72))
+    for i in range(2,10):
+        Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\tree.png', 1800+(i*100), 720+(9*72))
+    for i in range(0,10):
+        Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\tree.png', 1600, 720+(i*72))
+    for i in range(10,22):
+        Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\tree.png', 0, 72 * i)
+    for i in range(0,61):
+        Sprite(r'C:\Users\ravi4\OneDrive\Pictures\Saved Pictures\tree.png', 100*i, 1584)
